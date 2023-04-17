70b08d2f6e52cc17c810a563caf7bfa6103be548
edu_mendoza <emendozag1602@gmail.com>
2023-04-16 21:24:12 -0600

    Modified EmployeeService

--- src/main/java/com/ipn/escom/lit/hrmanager/service/EmployeeService.java
+++ src/main/java/com/ipn/escom/lit/hrmanager/service/EmployeeService.java
@@ -1,8 +1,14 @@
 package com.ipn.escom.lit.hrmanager.service;
 
 import com.ipn.escom.lit.hrmanager.exception.UserNotFoundException;
+import com.ipn.escom.lit.hrmanager.model.Departamento;
 import com.ipn.escom.lit.hrmanager.model.Employee;
+import com.ipn.escom.lit.hrmanager.model.Estado;
+import com.ipn.escom.lit.hrmanager.model.Puesto;
+import com.ipn.escom.lit.hrmanager.repo.DepartamentoRepo;
 import com.ipn.escom.lit.hrmanager.repo.EmployeeRepo;
+import com.ipn.escom.lit.hrmanager.repo.EstadoRepo;
+import com.ipn.escom.lit.hrmanager.repo.PuestoRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,14 +18,31 @@ import java.util.List;
 @Service
 public class EmployeeService {
     private final EmployeeRepo employeeRepo;
+    private final DepartamentoRepo departamentoRepo;
+    private final PuestoRepo puestoRepo;
+    private final EstadoRepo estadoRepo;
 
     @Autowired
-    public EmployeeService(EmployeeRepo employeeRepo) {
+    public EmployeeService(EmployeeRepo employeeRepo, DepartamentoRepo departamentoRepo, PuestoRepo puestoRepo, EstadoRepo estadoRepo) {
         this.employeeRepo = employeeRepo;
+        this.departamentoRepo = departamentoRepo;
+        this.puestoRepo = puestoRepo;
+        this.estadoRepo = estadoRepo;
     }
 
     public Employee addEmployee(Employee employee){
-//        employee.setEmployeeCode(UUID.randomUUID().toString());
+        // Obtener una instancia administrada de Departamento, puesto y estado
+        Departamento departamento = departamentoRepo.findDepartamentoById(employee.getDepartamento_id().getId())
+                .orElseThrow(() -> new UserNotFoundException("No se encontró el departamento con el ID proporcionado"));
+        Puesto puesto = puestoRepo.findPuestoById(employee.getPuesto_id().getId())
+                .orElseThrow(() -> new UserNotFoundException("No se encontró el puesto con el ID proporcionado"));
+        Estado estado = estadoRepo.findEstadoById(employee.getEstado_id().getId())
+                .orElseThrow(() -> new UserNotFoundException("No se encontró el estado con el ID proporcionado"));
+        // Asignar el departamento administrado a la entidad Employee
+
+        employee.setDepartamento_id(departamento);
+        employee.setPuesto_id(puesto);
+        employee.setEstado_id(estado);
         return employeeRepo.save(employee);
     }
 
